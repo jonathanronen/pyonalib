53e040bc5f9551a6f8bf0724b2e79210b087f0ea
Jona <yablee@gmail.com>
2015-10-14 16:23:35 +0200

    Skeleton setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyonalib',
+      version='0.1',
+      description="yona's python lib for things",
+      author='Jonathan Ronen',
+      license='GPLv2',
+      author_email='yableeatgmaildotcom',
+      url='',
+      packages=['pyonalib'],
+      install_requires=[
+          'six>=1.9.0',
+      ]
+     )
