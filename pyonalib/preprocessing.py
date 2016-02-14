666085747964afc386c0311649802439b649de7e
Jonathan Ronen <yablee@gmail.com>
2016-02-14 18:49:13 +0100

    Add docstring explaining dimensions to quantile_normalize

--- pyonalib/preprocessing.py
+++ pyonalib/preprocessing.py
@@ -4,6 +4,12 @@ from scipy import sparse, stats
 
 
 def quantile_normalize(F):
+    """
+    Quantile normalizes column-data matrix (features X samples matrix).
+    Each row in the input matrix represents a feature, and each column represents a sample.
+    The quantile-normalization makes sure every sample has the same *distribution* of feature weights.
+    Algorithm from wikipedia or something.
+    """
     if sparse.issparse(F):
         warn("Calling todense() on sparse matrix. This might crash your system.")
         F = F.todense()
