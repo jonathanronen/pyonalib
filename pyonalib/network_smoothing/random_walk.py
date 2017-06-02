8a47b8889d76ddf64c17989f50a53d12ae41b623
jona <yablee@gmail.com>
2017-06-02 18:45:49 +0200

    add 'transpose' argument with 'auto' option to deal with transposed expression matrices'

--- pyonalib/network_smoothing/random_walk.py
+++ pyonalib/network_smoothing/random_walk.py
@@ -21,15 +21,25 @@ from sklearn.preprocessing import normalize
 
 from scipy import io, linalg, sparse
 
-def smooth_by_iterations(adj_matrix, expr_matrix, alpha, tol=1e-9, max_iter=100):
+def _need_transpose(expr_matrix, adj_matrix):
+    """
+    The smoothing process is defined on a matrix of data rows. This checks the dimensions of E and A to see if E should be transposed.
+    """
+    return expr_matrix.shape[1] != adj_matrix.shape[0]
+
+def smooth_by_iterations(expr_matrix, adj_matrix, alpha, tol=1e-9, max_iter=100, transpose='auto'):
     """
     "Brute force" calculation of RWWR process.
 
         adj_matrix:    [N x N] matrix. Will be row-normalized.
         expr_matrix:   [M x N] DataFrame to be smoothed.
         alpha:         1 - the restart probability
-        tol, max_iter: iterative process will stop when change is < tol or max_iter is reached
+        tol, max_iter: Iterative process will stop when change is < tol or max_iter is reached
+        transpose:     If True, `expr_matrix` is [N x M] and will be transposed prior to smoothing and before returning. If 'auto', guess from dimensions.
     """
+    transpose = _need_transpose(expr_matrix, adj_matrix) if transpose=='auto' else transpose
+    if transpose:
+        expr_matrix = expr_matrix.T
     Anorm = np.matrix(normalize(adj_matrix, axis=1, norm='l1'))
     Fs = [expr_matrix]
     for i in range(1, max_iter+1):
@@ -37,7 +47,8 @@ def smooth_by_iterations(adj_matrix, expr_matrix, alpha, tol=1e-9, max_iter=100)
         if linalg.norm(Fs[-1] - Fs[-2]) < tol:
             print("Converged in {} iterations".format(i))
             break
-    return pd.DataFrame(Fs[-1], index=expr_matrix.index, columns=expr_matrix.columns)
+    ret = pd.DataFrame(Fs[-1], index=expr_matrix.index, columns=expr_matrix.columns)
+    return ret.T if transpose else ret
 
 def compute_smoothing_kernel(adj_matrix, alpha):
     """
@@ -62,20 +73,37 @@ def compute_smoothing_kernel(adj_matrix, alpha):
     influence_matrix = (1-alpha) * linalg.inv(I-alpha*Anorm)
     return influence_matrix
 
-def smooth_by_mtx_inv(adj_matrix, expr_matrix, alpha):
+def smooth_with_kernel(expr_matrix, kernel, transpose='auto'):
+    """
+    Perform smoothing of `expr_matrix` using provided kernel.
+
+            expr_matrix: [M x N] the data matrix to be smoothed.
+            kernel:      [N x N] smoothing kernel.
+            transpose:   If True, `expr_matrix` is [N x M] and will be transposed prior to smoothing and before returning. If 'auto', guess from dimensions.
+    """
+    transpose = _need_transpose(expr_matrix, kernel) if transpose=='auto' else transpose
+
+    if transpose:
+        return pd.DataFrame(np.dot(expr_matrix.T, K), index=expr_matrix.index,
+                           columns=expr_matrix.columns).T
+    else:
+        return pd.DataFrame(np.dot(expr_matrix, K), index=expr_matrix.index,
+                           columns=expr_matrix.columns)
+
+def smooth_by_mtx_inv(expr_matrix, adj_matrix, alpha, transpose='auto'):
     """
     Performs network smoothing of expr_matrix on the graph defined by adj_matrix using the closed form solution.
     See `compute_smoothing_kernel`.
 
-        adj_matrix:  [N x N] adjacency matrix defining the graph to smooth on. Will be row-normalized.
         expr_matrix: [M x N] the data matrix to be smoothed.
+        adj_matrix:  [N x N] adjacency matrix defining the graph to smooth on. Will be row-normalized.
         alpha:       1 - the restart probability.
+        transpose:   If True, `expr_matrix` is [N x M] and will be transposed prior to smoothing and before returning. If 'auto', guess from dimensions.
     """
     K = compute_smoothing_kernel(adj_matrix, alpha)
-    return pd.DataFrame(np.dot(expr_matrix, K), index=expr_matrix.index,
-                       columns=expr_matrix.columns)
+    return smooth_with_kernel(expr_matrix, K, transpose=transpose)
 
-def smooth_by_linalg_solve(adj_matrix, expr_matrix, alpha):
+def smooth_by_linalg_solve(expr_matrix, adj_matrix, alpha, transpose='auto'):
     """
     Performs network smoothing by RWWR by solving the matrix equation
 
@@ -85,11 +113,17 @@ def smooth_by_linalg_solve(adj_matrix, expr_matrix, alpha):
     but avoids inversions of a large matrix and is potentially both faster and more numerically stable,
     though different cases present different performance.
 
-        adj_matrix:  [N x N] adjacency matrix defining the graph to smooth on. Will be row-normalized.
         expr_matrix: [M x N] the data matrix to be smoothed.
+        adj_matrix:  [N x N] adjacency matrix defining the graph to smooth on. Will be row-normalized.
         alpha:       1 - the restart probability.
+        transpose:   If True, `expr_matrix` is [N x M] and will be transposed prior to smoothing and before returning. If 'auto', guess from dimensions.
     """
+    transpose = _need_transpose(expr_matrix, adj_matrix) if transpose=='auto' else transpose
+    if transpose:
+        expr_matrix = expr_matrix.T
     Anorm = normalize(adj_matrix, axis=1, norm='l1')
     I = np.eye(Anorm.shape[0])
     solution = linalg.solve((I-alpha*Anorm).T, expr_matrix.T*(1-alpha)).T
-    return pd.DataFrame(solution, index=expr_matrix.index, columns=expr_matrix.columns)
+
+    ret = pd.DataFrame(solution, index=expr_matrix.index, columns=expr_matrix.columns)
+    return ret.T if transpose else ret
