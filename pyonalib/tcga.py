8908022eef7d9ba5d8f793abd0fa4e3aa27ee260
jona <yablee@gmail.com>
2019-09-27 15:13:20 +0200

    Add functions to filter based on primary/recurrent tumor

--- pyonalib/tcga.py
+++ pyonalib/tcga.py
@@ -2,6 +2,7 @@
 Module for TCGA stuff
 """
 import pandas as pd
+
 # TCGA_CODE_TABLE = pd.DataFrame([
 #   ['01', 'Primary Solid Tumor', 'TP'],
 #   ['02', 'Recurrent Solid Tumor', 'TR'],
@@ -28,45 +29,82 @@ import pandas as pd
 #   ], columns=['Code', 'Definition', 'Short Letter Code']
 #   )
 
-CODE, DEFINITION, SHORT_CODE = list(zip(*[
-    ['01', 'Primary Solid Tumor', 'TP'],
-    ['02', 'Recurrent Solid Tumor', 'TR'],
-    ['03', 'Primary Blood Derived Cancer - Peripheral Blood', 'TB'],
-    ['04', 'Recurrent Blood Derived Cancer - Bone Marrow', 'TRBM'],
-    ['05', 'Additional - New Primary', 'TAP'],
-    ['06', 'Metastatic', 'TM'],
-    ['07', 'Additional Metastatic', 'TAM'],
-    ['08', 'Human Tumor Original Cells', 'THOC'],
-    ['09', 'Primary Blood Derived Cancer - Bone Marrow', 'TBM'],
-    ['10', 'Blood Derived Normal', 'NB'],
-    ['11', 'Solid Tissue Normal', 'NT'],
-    ['12', 'Buccal Cell Normal', 'NBC'],
-    ['13', 'EBV Immortalized Normal', 'NEBV'],
-    ['14', 'Bone Marrow Normal', 'NBM'],
-    ['15', 'sample type 15', '15SH'],
-    ['16', 'sample type 16', '16SH'],
-    ['20', 'Control Analyte', 'CELLC'],
-    ['40', 'Recurrent Blood Derived Cancer - Peripheral Blood', 'TRB'],
-    ['50', 'Cell Lines', 'CELL'],
-    ['60', 'Primary Xenograft Tissue', 'XP'],
-    ['61', 'Cell Line Derived Xenograft Tissue', 'XCL'],
-    ['99', 'sample type 99', '99SH'],
-    ]))
+CODE, DEFINITION, SHORT_CODE = list(
+    zip(
+        *[
+            ["01", "Primary Solid Tumor", "TP"],
+            ["02", "Recurrent Solid Tumor", "TR"],
+            ["03", "Primary Blood Derived Cancer - Peripheral Blood", "TB"],
+            ["04", "Recurrent Blood Derived Cancer - Bone Marrow", "TRBM"],
+            ["05", "Additional - New Primary", "TAP"],
+            ["06", "Metastatic", "TM"],
+            ["07", "Additional Metastatic", "TAM"],
+            ["08", "Human Tumor Original Cells", "THOC"],
+            ["09", "Primary Blood Derived Cancer - Bone Marrow", "TBM"],
+            ["10", "Blood Derived Normal", "NB"],
+            ["11", "Solid Tissue Normal", "NT"],
+            ["12", "Buccal Cell Normal", "NBC"],
+            ["13", "EBV Immortalized Normal", "NEBV"],
+            ["14", "Bone Marrow Normal", "NBM"],
+            ["15", "sample type 15", "15SH"],
+            ["16", "sample type 16", "16SH"],
+            ["20", "Control Analyte", "CELLC"],
+            ["40", "Recurrent Blood Derived Cancer - Peripheral Blood", "TRB"],
+            ["50", "Cell Lines", "CELL"],
+            ["60", "Primary Xenograft Tissue", "XP"],
+            ["61", "Cell Line Derived Xenograft Tissue", "XCL"],
+            ["99", "sample type 99", "99SH"],
+        ]
+    )
+)
+
 
 def barcode2sampcode(barcode):
-    return barcode.split('-')[3][:2]
+    return barcode.split("-")[3][:2]
+
 
 CODE2DEF = {code: defi for code, defi in zip(CODE, DEFINITION)}
+
+
 def barcode2samptype(barcode):
     return CODE2DEF[barcode2sampcode(barcode)]
 
 
-TUMOR_SAMPLE_CODES = { '01', '02', '03', '04', '05', '09', '40' }
+TUMOR_SAMPLE_CODES = {"01", "02", "03", "04", "05", "09", "40"}
+
+
 def select_tumor_samples(df):
     tumor_samples = [e for e in df.index if barcode2sampcode(e) in TUMOR_SAMPLE_CODES]
     return df.loc[tumor_samples]
 
-NORMAL_SAMPLE_CODES = { '10', '11', '12', '13', '14' }
+
+NORMAL_SAMPLE_CODES = {"10", "11", "12", "13", "14"}
+
+
 def select_normal_samples(df):
     normal_samples = [e for e in df.index if barcode2sampcode(e) in NORMAL_SAMPLE_CODES]
     return df.loc[normal_samples]
+
+
+PRIMARY_TUMOR_CODES = {
+    code for code, defn in zip(CODE, DEFINITION) if "Primary" in defn
+}
+
+
+def select_primary_tumor_samples(df):
+    primary_tumor_samples = [
+        e for e in df.index if barcode2sampcode(e) in PRIMARY_TUMOR_CODES
+    ]
+    return df.loc[primary_tumor_samples]
+
+
+RECURRENT_TUMOR_CODES = {
+    code for code, defn in zip(CODE, DEFINITION) if "Recurrent" in defn
+}
+
+
+def select_recurrent_tumor_samples(df):
+    recurrent_tumor_samples = [
+        e for e in df.index if barcode2sampcode(e) in RECURRENT_TUMOR_CODES
+    ]
+    return df.loc[recurrent_tumor_samples]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyonalib',
-      version='0.1.2',
+      version='0.1.3',
       description="yona's python lib for things",
       author='Jonathan Ronen',
       license='GPLv2',
